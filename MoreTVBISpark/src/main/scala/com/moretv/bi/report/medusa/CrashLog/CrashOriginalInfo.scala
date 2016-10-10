b1e66f7a36739de35f76b2f5f9f84f1db8c9b497
witnessmg <zealotwit@gmail.com>
2016-10-10 16:52:09 +0800

    init

---
+++ MoreTVBISpark/src/main/scala/com/moretv/bi/report/medusa/CrashLog/CrashOriginalInfo.scala
@@ -0,0 +1,93 @@
+package com.moretv.bi.report.medusa.CrashLog
+
+/**
+ * Created by xiajun on 2016/3/28.
+ * 该对象用于获取每天的crash的原始数据
+ */
+import java.lang.{Long => JLong}
+
+import com.moretv.bi.medusa.util.DevMacUtils
+import com.moretv.bi.util.baseclasee.{ModuleClass, BaseClass}
+import com.moretv.bi.util.{ParamsParseUtil, SparkSetting, _}
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.storage.StorageLevel
+import org.json.JSONObject
+import com.moretv.bi.medusa.util.ParquetDataStyle.ALL_CRASH_INFO_INCLUDE_MD5
+
+
+object CrashOriginalInfo extends BaseClass{
+
+
+  def main(args: Array[String]) {
+    ModuleClass.executor(CrashOriginalInfo,args)
+  }
+
+  override def execute(args: Array[String]) {
+    ParamsParseUtil.parse(args) match {
+      case Some(p) =>{
+        val s = sqlContext
+        import s.implicits._
+        val util = new DBOperationUtils("medusa")
+        val inputDate = p.startDate
+        val day = DateFormatUtils.toDateCN(inputDate)
+        val logRdd = sc.textFile(s"/log/crash/metadata/${inputDate}_extraction.log").map(log=>{
+          val json = new JSONObject(log)
+          (json.optString("fileName"),json.optString("MAC"),json.optString("APP_VERSION_NAME"),json.optString("APP_VERSION_CODE"),
+            json.optString("ANDROID_VERSION"),json.optString("STACK_TRACE"),json.optString("DATE_CODE"),json.optString("PRODUCT_CODE"))
+        })
+
+        val filterRdd = logRdd.map(log => (log._1,log._2.replace(":",""),log._3,log._4,log._5,log._6,log._7,log._8))
+          .filter(data => !DevMacUtils.macFilter(data._2))
+
+        /**
+         * Create a new DF which including the md5
+         */
+        filterRdd.map(e=>(e._1,DigestUtils.md5Hex(e._1),e._2,e._3,e._4,e._5,e._6,DigestUtils.md5Hex(e._6),e._7,e._8)).
+          toDF("fileName","fileName_md5","mac","appVersionName","appVersionCode","androidVersion","stackTrace","stackTraceMD5",
+          "dateCode","productCode").registerTempTable("crashInfo_MD5")
+
+        /**
+         * The statistic process for different needs
+         *
+         */
+
+        val rdd = sqlContext.sql("select appVersionName, androidVersion,dateCode,productCode,stackTraceMD5,stackTrace from" +
+          " crashInfo_MD5").map(e=>(e.getString(0),e.getString(1),e.getString(2),e.getString(3),e.getString(4),e.getString(5)))
+
+        // Getting the number of each version/crash_key/stack_trace
+        val crashNumRdd = sqlContext.sql("select appVersionName, androidVersion,dateCode,productCode,stackTraceMD5," +
+          "stackTrace,count(fileName_md5) from crashInfo_MD5 group by appVersionName, androidVersion,dateCode,productCode," +
+          "stackTraceMD5,stackTrace").map(e=>(e.getString(0),e.getString(1),e.getString(2),e.getString(3),e.getString(4),
+          e.getString(5),e.getLong(6)))
+
+        val merger = rdd.distinct().map(e=>((e._1,e._2,e._3,e._4,e._5,e._6),1.toLong)) join(crashNumRdd.map(e=>((e._1,e._2,e
+          ._3,e._4,e._5,e._6),(e._7))))
+
+        val metaData = merger.map(e=>(e._1._1, e._1._2, e._1._3, e._1._4, e._1._5,e._1._6,e._2._2)).distinct()
+          .collect()
+        // Insert data into database
+        val insert_sql = "INSERT INTO medusa_crash_original_secondary_phase_info(day,app_version_name,android_version," +
+          "date_code,product_code,stack_trace_md5,stack_trace,crash_num) VALUES(?,?,?,?,?,?,?,?)"
+        if(p.deleteOld) {
+          val date = DateFormatUtils.toDateCN(p.startDate, -1)
+          val oldSql = s"delete from medusa_crash_original_secondary_phase_info where day = '$date'"
+          util.delete(oldSql)
+        }
+        metaData.foreach(e=>{
+          try{
+            util.insert(insert_sql,day,e._1,e._2,e._3,e._4,e._5,e._6,new JLong(e._7))
+          }catch{
+            case e:Exception=> e.printStackTrace()
+          }
+
+        })
+
+        rdd.unpersist()
+      }
+      case None => {throw new RuntimeException("At least need one param: --startDate")}
+    }
+  }
+}
+
---
+++ MoreTVBISpark/src/main/scala/com/moretv/bi/report/medusa/functionalStatistic/searchInfo/SearchEntranceStat.scala
@@ -0,0 +1,98 @@
+package com.moretv.bi.report.medusa.functionalStatistic.searchInfo
+
+import java.lang.{Long => JLong}
+import java.util.Calendar
+
+import com.moretv.bi.util.baseclasee.{BaseClass, ModuleClass}
+import com.moretv.bi.util.{DBOperationUtils, DateFormatUtils, ParamsParseUtil}
+
+/**
+  * Created by witnes on 9/18/16.
+  */
+
+/**
+  * 领域: 搜索
+  * 对象: 搜索入口
+  * 数据源: searchEntrance
+  * 维度: 天, 入口
+  * 特征提取: entrance, userId
+  * 过滤条件:
+  * 统计: 入口 , pv, uv
+  * 输出: tbl[ search_entrance_stat ]
+  *         (day, entrance, pv, uv)
+  */
+object SearchEntranceStat extends BaseClass{
+
+  private val dataSource = "searchEntrance"
+
+  private val tableName = "search_entrance_stat"
+
+  private val fields = "day, entrance, pv, uv"
+
+  private val insertSql = s"insert into $tableName($fields) values(?,?,?,?)"
+
+  private val deleteSql = s"delete from $tableName where day = ?"
+
+  def main(args: Array[String]) {
+
+    ModuleClass.executor(SearchEntranceStat,args)
+
+  }
+
+  override def execute(args: Array[String]): Unit = {
+
+    ParamsParseUtil.parse(args) match {
+      case Some(p) => {
+        //init & util
+        val util = new DBOperationUtils("medusa")
+        //date
+        val startDate = p.startDate
+        val cal = Calendar.getInstance
+        cal.setTime(DateFormatUtils.readFormat.parse(startDate))
+
+        (0 until p.numOfDays).foreach(w=>{
+          //date
+          val loadDate = DateFormatUtils.readFormat.format(cal.getTime)
+          cal.add(Calendar.DAY_OF_MONTH,-1)
+          val sqlDate = DateFormatUtils.cnFormat.format(cal.getTime)
+
+          //path
+          val loadPath = s"/log/medusa/parquet/$loadDate/$dataSource"
+          println(loadPath)
+
+          //df
+          val df = sqlContext.read.parquet(loadPath)
+                      .select("userId","entrance")
+                      .filter("entrance is not null")
+          //rdd
+          val rdd = df.map(e=>(e.getString(1),e.getString(0)))
+                      .cache
+
+          //aggregate
+          val pvMap = rdd.countByKey
+          val uvMap = rdd.distinct.countByKey
+
+          //deal with table
+          util.delete(deleteSql, sqlDate)
+
+          uvMap.foreach(w=>{
+
+            val key = w._1
+            val uv = w._2
+            val pv = pvMap.get(key) match {
+              case Some(p) => p
+              case _ => 0
+            }
+            util.insert(insertSql, sqlDate, key, new JLong(pv), new JLong(uv))
+
+          })
+
+        })
+      }
+      case None => {
+        throw new Exception("SearchEntranceStat fails")
+      }
+
+    }
+  }
+}
