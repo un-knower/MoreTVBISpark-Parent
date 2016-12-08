cdfda4c9a0e2071a95ade95c7952a58429761948
xiajun <xia.jun@whaley.cn>
2016-12-08 13:31:42 +0800

    增加文件

--- MoreTVBISpark/src/main/scala/com/moretv/bi/util/Params.scala
+++ MoreTVBISpark/src/main/scala/com/moretv/bi/util/Params.scala
@@ -1,22 +1,31 @@
 package com.moretv.bi.util
 
 /**
-  * Created by laishun on 15/10/12.
-  */
+ * Created by laishun on 15/10/12.
+ */
 
-case class Params(startDate: String = "yyyyMMdd", //起始日期,格式为yyyyMMdd
-                  endDate: String = "yyyyMMdd", //结束日期,格式为yyyyMMdd
-                  startTime: String = "HH:mm:ss", //开始时间，格式为HH:mm:ss
-                  endTime: String = "HH:mm:ss", //结束时间，格式为HH:mm:ss
-                  whichMonth: String = "", //执行的月份,格式为yyyyMM
-                  numOfDays: Int = 1, //天数
-                  deleteOld: Boolean = false, //是否删除旧数据
-                  logType: String = "", //单个日志类型
-                  srcPath: String = "", // 数据源路径
-                  statXml: String = "", //用于做统计xml文件
-                  applicationMode: String = "medusa", // 区分哪一个应用
-                  logTypes: String = "", //多个日志类型，以英文逗号(,)分割
-                  alarmFlag: Boolean = true, //限定版本号
-                  outputFile: String = "" //输出csv文件路径
-                 )
-
+case class Params (startDate:String ="yyyyMMdd", //起始日期,格式为yyyyMMdd
+                   endDate: String = "yyyyMMdd", //结束日期,格式为yyyyMMdd
+                   whichMonth:String = "", //执行的月份,格式为yyyyMM
+                   deleteOld:Boolean=false, //是否删除旧数据
+                   logType:String = "", //单个日志类型
+                   columns:String = "", //多个列名，以英文逗号(,)分割
+                   fileDir:String = "", // 数据所在的目录
+//                   applicationMode:String = "medusa", // 区分哪一个应用
+                   logTypes:String = "", //多个日志类型，以英文逗号(,)分割
+                   srcPath: String = "", // 数据源路径
+//                   statXml: String = "", //用于做统计xml文件
+//                   database:String = "", //插入数据到哪个数据库
+//                   startTime:String = "HH:mm:ss", //开始时间，格式为HH:mm:ss
+//                   endTime:String = "HH:mm:ss", //结束时间，格式为HH:mm:ss
+//                   sid:String = "", //节目的sid
+                   whichDay:Int = 1, //
+                   numOfDays:Int = 1, //天数
+                   offset:Int = 1,//偏移量
+                   apkVersion:String = "",
+                   durationMax:Int  = 0, //播放时长区间最大值
+                   alarmFlag:Boolean = true,  //限定版本号
+//                   contentType:String = "",
+//                   dateInfo:String="" ,
+                   outputFile: String = "" //输出csv文件路径
+                    )//节目类型
--- MoreTVBISpark/src/main/scala/com/moretv/bi/util/ParamsParseUtil.scala
+++ MoreTVBISpark/src/main/scala/com/moretv/bi/util/ParamsParseUtil.scala
@@ -5,79 +5,76 @@ import java.text.SimpleDateFormat
 import scopt.OptionParser
 
 /**
-  * Created by Will on 2015/9/28.
-  */
+ * Created by Will on 2015/9/28.
+ */
 object ParamsParseUtil {
 
   private val default = Params()
   private val readFormat = DateFormatUtils.readFormat
   private val timeFormat = new SimpleDateFormat("HH:mm:ss")
 
-  def parse(args: Seq[String], default: Params = default) = {
-    if (args.nonEmpty) {
-      val parser = new OptionParser[Params]("ParamsParse") {
-        head("ParamsParse", "1.2")
+  def parse(args: Seq[String],default:Params = default) = {
+      if(args.nonEmpty){
+        val parser = new OptionParser[Params]("ParamsParse") {
+          head("ParamsParse","1.2")
+          opt[String]("excuteDate").action((x,c)=>c.copy(startDate=x)).
+            validate(e => try {
+              readFormat.parse(e)
+              success
+            }catch {
+              case e:Exception => failure("wrong date format, should be 'yyyyMMdd'")
+            })
+          opt[String]("startDate").action((x,c)=>c.copy(startDate=x)).
+            validate(e => try {
+              readFormat.parse(e)
+              success
+            }catch {
+              case e:Exception => failure("wrong date format, should be 'yyyyMMdd'")
+            })
+          opt[Boolean]("deleteOld").action((x,c)=>c.copy(deleteOld = x))
+          opt[String]("apkVersion").action((x,c)=>c.copy(apkVersion = x))
+          opt[String]("columns").action((x,c)=>c.copy(columns=x))
+          opt[String]("logTypes").action((x,c)=>c.copy(logTypes=x))
+          opt[String]("logType").action((x,c)=>c.copy(logType=x))
+          opt[String]("startDate").action((x,c)=>c.copy(startDate=x))
+          opt[String]("srcPath").action((x,c)=>c.copy(srcPath=x))
+//          opt[String]("statXml").action((x,c)=>c.copy(statXml=x))
+          opt[String]("outputFile").action((x,c)=>c.copy(outputFile=x))
+//          opt[String]("database").action((x,c)=>c.copy(database=x))
+//          opt[String]("contentType").action((x,c)=>c.copy(contentType=x))
+          opt[String]("whichMonth").action((x,c)=>c.copy(whichMonth=x))
+          opt[Boolean]("alarmFlag").action((x,c)=>c.copy(alarmFlag = x))
+          opt[Int]("durationMax").action((x,c)=>c.copy(durationMax=x))
+          opt[Int]("numOfDays").action((x,c)=>c.copy(numOfDays=x)).
+            validate(e => {
+              if(e > 0) success else failure("numOfDays must be bigger than 0")
+            })
+          opt[Int]("whichDay").action((x,c)=>c.copy(whichDay=x))
+          opt[String]("endDate").action((x, c) => c.copy(endDate = x))
+//          opt[String]("startTime").action((x,c)=>c.copy(startTime=x)).
+//            validate(e => try {
+//              timeFormat.parse(e)
+//              success
+//            }catch {
+//              case e:Exception => failure("wrong time format, should be 'HH:mm:ss'")
+//            })
+//          opt[String]("endTime").action((x,c)=>c.copy(endTime=x)).
+//            validate(e => try {
+//              timeFormat.parse(e)
+//              success
+//            }catch {
+//              case e:Exception => failure("wrong time format, should be 'HH:mm:ss'")
+//            })
 
-        opt[String]("excuteDate").action((x, c) => c.copy(startDate = x)).
-          validate(e => try {
-            readFormat.parse(e)
-            success
-          } catch {
-            case e: Exception => failure("wrong date format, should be 'yyyyMMdd'")
-          })
-
-        opt[String]("startDate").action((x, c) => c.copy(startDate = x)).
-          validate(e => try {
-            readFormat.parse(e)
-            success
-          } catch {
-            case e: Exception => failure("wrong date format, should be 'yyyyMMdd'")
-          })
-
-        opt[Boolean]("deleteOld").action((x, c) => c.copy(deleteOld = x))
-
-        opt[String]("logTypes").action((x, c) => c.copy(logTypes = x))
-
-        opt[String]("logType").action((x, c) => c.copy(logType = x))
-
-        opt[String]("startDate").action((x, c) => c.copy(startDate = x))
-
-        opt[String]("endDate").action((x, c) => c.copy(endDate = x))
-
-        opt[String]("whichMonth").action((x, c) => c.copy(whichMonth = x))
-
-        opt[Boolean]("alarmFlag").action((x, c) => c.copy(alarmFlag = x))
-
-        opt[Int]("numOfDays").action((x, c) => c.copy(numOfDays = x)).
-          validate(e => {
-            if (e > 0) success else failure("numOfDays must be bigger than 0")
-          })
-
-        opt[String]("startTime").action((x, c) => c.copy(startTime = x)).
-          validate(e => try {
-            timeFormat.parse(e)
-            success
-          } catch {
-            case e: Exception => failure("wrong time format, should be 'HH:mm:ss'")
-          })
-
-        opt[String]("endTime").action((x, c) => c.copy(endTime = x)).
-          validate(e => try {
-            timeFormat.parse(e)
-            success
-          } catch {
-            case e: Exception => failure("wrong time format, should be 'HH:mm:ss'")
-          })
-
-        opt[String]("outputFile").action((x, c) => c.copy(outputFile = x))
-
-        opt[String]("srcPath").action((x, c) => c.copy(srcPath = x))
-      }
-      parser.parse(args, default)
-    } else Some(default)
+//          opt[String]("sid").action((x,c)=>c.copy(sid=x))
+          opt[Int]("offset").action((x,c)=>c.copy(offset=x))
+//          opt[String]("dateInfo").action((x,c)=> c.copy(dateInfo=x))
+        }
+        parser.parse(args,default)
+      }else Some(default)
   }
 
-  def withParse(args: Seq[String])(f: Params => Unit) = {
+  def withParse(args:Seq[String])(f : Params => Unit) = {
     parse(args) match {
       case Some(p) => f(p)
       case None =>
