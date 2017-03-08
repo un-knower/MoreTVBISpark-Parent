aa704db1ed157635b021840e1fd45bb5a70da252
xia.jun@whaley.cn <xia.jun@whaley.cn>
2017-03-08 20:25:05 +0800

    throw exception

--- src/main/scala/com/moretv/bi/report/medusa/CrashLog/EachUserEachCrashAppearInfo.scala
+++ src/main/scala/com/moretv/bi/report/medusa/CrashLog/EachUserEachCrashAppearInfo.scala
@@ -24,55 +24,39 @@ object EachUserEachCrashAppearInfo extends BaseClass{
   override def execute(args: Array[String]) {
     ParamsParseUtil.parse(args) match {
       case Some(p) =>{
-        val s = sqlContext
-        import s.implicits._
         val util = DataIO.getMySqlOps(DataBases.MORETV_MEDUSA_MYSQL)
         val inputDate = p.startDate
         val day = DateFormatUtils.toDateCN(inputDate,-1)
-        // TODO 是否需要写到固定的常量类或者SDK读取
-        val logRdd = DataIO.getDataFrameOps.getDF(sc,p.paramMap,MEDUSA,LogTypes.CRASH_LOG,inputDate).select(
-          "MAC","APP_VERSION_NAME","APP_VERSION_NAME","ANDROID_VERSION","STACK_TRACE","DATE_CODE","PRODUCT_CODE","CUSTOM_JSON_DATA"
-        ).map(e=>(e.getString(0),e.getString(1),e.getString(2),e.getString(3),e.getString(4),e.getString(5),e.getString(6),e.getString(7)))
-
-        val filterRdd = logRdd.repartition(28).map(log => (log._1.replace(":",""),log._2,log._3,log._4,log._5,log._6,log._7,log._8))
-          .filter(data => !DevMacUtils.macFilter(data._1))
-
-        /**
-         * Create a new DF which including the md5
-         */
-        filterRdd.map(e=>(e._1,e._2,e._3,e._4,e._5,DigestUtils.md5Hex(e._5),e._6,e._7,e._8)).
-          toDF("mac","appVersionName","appVersionCode","androidVersion","stackTrace","stackTraceMD5",
-          "dateCode","productCode","customJsonData").registerTempTable("crashInfo_MD5")
-
-        /**
-         * The statistic process for different needs
-         *
-         */
-
-        val rdd = sqlContext.sql("select stackTraceMD5,mac,productCode,dateCode," +
-          "count(1) " +
-          "from crashInfo_MD5 group by stackTraceMD5,mac,productCode,dateCode").
-          map(e=>(e.getString(0),e.getString(1),e.getString(2),e.getString(3),e.getLong(4)))
-
         val insertSql = "insert into tmp_crash_info(day,mac,stackTraceMD5,productCode,dateCode,num) values(?,?,?,?,?,?)"
-
+        // TODO 是否需要写到固定的常量类或者SDK读取
         if(p.deleteOld){
           val deleteSql = "delete from tmp_crash_info where day = ?"
           util.delete(deleteSql,day)
         }
-        rdd.foreachPartition(partition=>{
-          val util1 = DataIO.getMySqlOps(DataBases.MORETV_MEDUSA_MYSQL)
-          partition.foreach(r=>{
-            try{
-              util1.insert(insertSql,day,r._2,r._1,r._3,r._4,new JLong(r._5))
-            }catch{
-              case e =>
-            }
-          })
+//        DataIO.getDataFrameOps.getDF(sc,p.paramMap,MEDUSA,LogTypes.CRASH_LOG,inputDate).select(
+//          "MAC","APP_VERSION_NAME","APP_VERSION_NAME","ANDROID_VERSION","STACK_TRACE","DATE_CODE","PRODUCT_CODE","CUSTOM_JSON_DATA"
+//        ).map(e=>(e.getString(0),e.getString(1),e.getString(2),e.getString(3),e.getString(4),e.getString(5),e.getString(6),e.getString(7))).
+//          map(log => (log._1.replace(":",""),log._2,log._3,log._4,log._5,log._6,log._7,log._8))
+//          .filter(data => !DevMacUtils.macFilter(data._1)).map(e=>((DigestUtils.md5Hex(e._5),e._1,e._7,e._6),1)).
+//          countByKey().foreach(e=>{
+//          util.insert(insertSql,day,e._1._1,e._1._2,e._1._3,e._1._4,new JLong(e._2))
+//        })
+
+        DataIO.getDataFrameOps.getDF(sc,p.paramMap,MEDUSA,LogTypes.CRASH_LOG,inputDate).select(
+          "MAC","STACK_TRACE","DATE_CODE","PRODUCT_CODE"
+        ).map(e=>(e.getString(0),e.getString(1),e.getString(2),e.getString(3))).
+          map(log => (log._1.replace(":",""),log._2,log._3,log._4))
+          .filter(data => !DevMacUtils.macFilter(data._1)).filter(_._1!=null).filter(_._2!=null).filter(_._3!=null).
+          filter(_._4!=null)
+          .map(e=>((DigestUtils.md5Hex(e._2),e._1,e._4,e._3),1)).
+          countByKey().foreach(e=>{
+          util.insert(insertSql,day,e._1._1,e._1._2,e._1._3,e._1._4,new JLong(e._2))
         })
+
+
+
       }
       case None => {throw new RuntimeException("At least need one param: --startDate")}
     }
   }
 }
-
--- src/main/scala/com/moretv/bi/util/baseclasee/ModuleClass.scala
+++ src/main/scala/com/moretv/bi/util/baseclasee/ModuleClass.scala
@@ -49,6 +49,7 @@ object ModuleClass {
         if(alarmFlag){
           ExceptionManage.taskExceptionStore(op.getClass.getName,e,executeTime)
         }
+        throw e
       }
     }finally{
       op.destroy()
