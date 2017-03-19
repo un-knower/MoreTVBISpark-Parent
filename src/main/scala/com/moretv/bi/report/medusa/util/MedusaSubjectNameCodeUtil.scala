10738588f4e841b53e75573f5b6d36d66862a608
wang.baozhi <286790238@qq.com>
2017-03-19 14:23:45 +0800

    解析出所有分析脚本可以使用的subject code

--- src/main/scala/com/moretv/bi/report/medusa/util/MedusaSubjectNameCodeUtil.scala
+++ src/main/scala/com/moretv/bi/report/medusa/util/MedusaSubjectNameCodeUtil.scala
@@ -4,7 +4,6 @@ package com.moretv.bi.report.medusa.util
  * Created by xiajun on 2016/8/1.
  * 该util用于处理pathSpecial中的subject-专题名称-专题code与subject-专题名称的情况
   *
-  * 残言化为句点，灯火黯然泯灭，忽见恍若隔世的你 不负沉默不言的抱歉 不见往日共度雨缠绵
  */
 object MedusaSubjectNameCodeUtil {
   //private val regex="""(movie|tv|hot|kids|zongyi|comic|jilu|sports|xiqu|mv)([0-9]+)""".r
