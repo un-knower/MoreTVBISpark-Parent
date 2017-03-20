3f3ac9bdfd7519755e258c24eea303cad1b586fb
wang.baozhi <286790238@qq.com>
2017-03-20 11:23:50 +0800

    解析出所有分析脚本可以使用的subject code

--- src/main/scala/com/moretv/bi/report/medusa/util/MedusaSubjectNameCodeUtil.scala
+++ src/main/scala/com/moretv/bi/report/medusa/util/MedusaSubjectNameCodeUtil.scala
@@ -7,7 +7,9 @@ package com.moretv.bi.report.medusa.util
  */
 object MedusaSubjectNameCodeUtil {
   //private val regex="""(movie|tv|hot|kids|zongyi|comic|jilu|sports|xiqu|mv)([0-9]+)""".r
-  private val regex_etl="""(movie|tv|hot|kids|zongyi|comic|jilu|sports|xiqu|mv|kid)([0-9]+)""".r
+  //在正确性上需要加上kid，现在保证正确性去掉kid正则.或者以后使用([a-z]+)([0-9]+)正则表达式
+  private val regex_etl="""(movie|tv|hot|kids|zongyi|comic|jilu|sports|xiqu|mv)([0-9]+)""".r
+  // private val regex_etl="""(movie|tv|hot|kids|zongyi|comic|jilu|sports|xiqu|mv|kid)([0-9]+)""".r
   private val regexSubjectName="""subject-([a-zA-Z0-9-\u4e00-\u9fa5]+)""".r
   // 获取 专题code
   def getSubjectCode(subject:String) = {
@@ -33,8 +35,15 @@ object MedusaSubjectNameCodeUtil {
   }
 
   def main(args: Array[String]) {
-    val pathSpecial="subject-儿歌一周热播榜"
-    println(getSubjectNameETL(pathSpecial))
+    val pathSpecial="subject-儿歌一周热播榜-julu11"
+    val regex_etl="""([a-z]+)([0-9]+)""".r
+    regex_etl findFirstMatchIn pathSpecial match {
+      case Some(m) =>{
+        println(m.group(1)+m.group(2))
+      }
+      case None =>  println("abc")
+    }
+    //println(getSubjectNameETL(pathSpecial))
   }
 
 
