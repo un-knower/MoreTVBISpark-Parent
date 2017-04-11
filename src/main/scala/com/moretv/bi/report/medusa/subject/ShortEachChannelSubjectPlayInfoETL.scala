8d595e6d095290c8b101e6e240cd913bf0d7d8f6
wang.baozhi <286790238@qq.com>
2017-04-11 12:03:09 +0800

    使用事实表关联维度表计算专题播放量

--- src/main/scala/com/moretv/bi/report/medusa/subject/ShortEachChannelSubjectPlayInfoETL.scala
+++ src/main/scala/com/moretv/bi/report/medusa/subject/ShortEachChannelSubjectPlayInfoETL.scala
@@ -15,21 +15,12 @@ import org.apache.spark.sql.DataFrame
   * Created by baozhi.wang on 2017/3/31.
   * 脚本作用：统计不同频道的专题播放量，用于展示在各个频道的专题播放趋势图以及内容评估的专题趋势图
   *
-  * 逻辑方式【与EachChannelSubjectPlayInfo代码比较】：
-  * 首先从日志里获取subject code，如果subject code为null,通过subject name关联维度表dim_medusa_subject获得subject code
-  * 最终获得userId,subject code两个字段作为分析表
-  * 最终目的：通过分析表和维度表dim_medusa_subject关联，获取分析结果
-  *
-  * 在下面注释的代码块内逻辑， 以后会迁移，用来做事实表ETL操作：
-  * /*最终此逻辑会合并进入事实表的ETL过程-start*/
-  * /*最终此逻辑会合并进入事实表的ETL过程-end*/
-  * 事实表ETL目的：将日志一次性地完成ETL生成FACT_TABLE，之后其他分析脚本直接使用FACT_TABLE简单地获取想要分析的字段，
-  * 例如：本脚本可以直接从FACT_TABLE获得userId,subjectCode进行分析，而不需在每个关于专题分析的脚本里，
-  * 重复的从原来的MERGER表获得subjectCode分析字段。
+  * 逻辑方式【与EachChannelSubjectPlayInfoETL代码比较】：
+    使用事实表与维度表关联，直接获得分析结果
   */
 object ShortEachChannelSubjectPlayInfoETL extends BaseClass {
   private val mysql_analyze_result_table = "medusa_channel_subject_play_info"
-  private val spark_df_analyze_table = "step3_table"
+  private val spark_df_analyze_table = "spark_df_analyze_table"
   private val isDebug = false
 
   def main(args: Array[String]): Unit = {
@@ -57,7 +48,7 @@ object ShortEachChannelSubjectPlayInfoETL extends BaseClass {
           val date = DateFormatUtils.readFormat.format(calendar.getTime)
           val insertDate = DateFormatUtils.toDateCN(date, -1)
           calendar.add(Calendar.DAY_OF_MONTH, -1)
-          DataIO.getDataFrameOps.getDF(sqlContext, p.paramMap, MERGER, LogTypes.PLAY_VIEW_2_FILTER_ETL, date).registerTempTable(spark_df_analyze_table)
+          DataIO.getDataFrameOps.getDF(sqlContext, p.paramMap, MERGER, LogTypes.PLAY_VIEW_ETL, date).registerTempTable(spark_df_analyze_table)
 
           /*进入分析代码*/
           if (p.deleteOld) {
