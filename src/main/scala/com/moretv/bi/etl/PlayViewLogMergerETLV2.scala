2028de7180ccc44584f02a74febbb116df1c6967
wang.baozhi <286790238@qq.com>
2017-04-12 16:01:45 +0800

    playview-etl log generate online

---
+++ src/main/scala/com/moretv/bi/etl/PlayViewLogMergerETLV2.scala
@@ -0,0 +1,234 @@
+package com.moretv.bi.etl
+
+import java.util.Calendar
+
+import cn.whaley.sdk.dataexchangeio.DataIO
+import com.moretv.bi.global.{DimensionTypes, LogTypes}
+import com.moretv.bi.report.medusa.util.FilesInHDFS
+import com.moretv.bi.util._
+import com.moretv.bi.util.baseclasee.{BaseClass, ModuleClass}
+
+
+/**
+  * Created by baozhi.wang on 2017/3/14.
+  * This object is used to merge the parquet data of medusa and moretv into one parquet!
+  * input: /log/medusa/parquet/$date/play
+  * input: /mbi/parquet/playview/$date
+  * output: /log/medusaAndMoretv/parquet/$date/playviewETL
+  *
+  * play事实表ETL需要做的任务：
+  * 1.解析subject code维度【参考com.moretv.bi.report.medusa.subject.EachChannelSubjectPlayInfoETL代码】
+  * 2.解析列表页维度【
+  *       普通类型：参考com.moretv.bi.report.medusa.channelClassification.ChannelClassificationStatETL
+  *       少儿类型：参考com.moretv.bi.logETL.KidsPathParser
+  *       音乐类型：参考佳莹代码
+  *       体育类型：参考com.moretv.bi.logETL.SportsPathParser】
+  * 3.解析首页入口维度
+  *   参考：com.moretv.bi.report.medusa.entrance.ChannelEntrancePlayStatETL
+  *
+  * 过滤播放量大于5000的记录【参考com.moretv.bi.report.medusa.subject.EachChannelSubjectPlayInfoETL代码】
+  * 移动到其他类做，保留最原始数据，方便更改规则后批量刷数据
+  *
+  */
+object PlayViewLogMergerETLV2 extends BaseClass {
+  private val playNumLimit = 5000
+
+  def main(args: Array[String]) {
+    ModuleClass.executor(this, args)
+  }
+
+  override def execute(args: Array[String]) {
+    ParamsParseUtil.parse(args) match {
+      case Some(p) => {
+        sqlContext.udf.register("getSubjectCode", PathParserETL.getSubjectCodeByPathETL _)
+        sqlContext.udf.register("getSubjectName", PathParserETL.getSubjectNameByPathETL _)
+        sqlContext.udf.register("getEntranceType", PathParserETL.getEntranceTypeByPathETL _)
+        sqlContext.udf.register("getListCategoryMedusa", PathParserETL.getListCategoryMedusaETL _)
+        sqlContext.udf.register("getListCategoryMoretv", PathParserETL.getListCategoryMoretvETL _)
+
+        //引入维度表
+        val dimension_source_site_input_dir = DataIO.getDataFrameOps.getDimensionPath(MEDUSA_DIMENSION, DimensionTypes.DIM_MEDUSA_SOURCE_SITE)
+        val dimensionSourceSiteFlag = FilesInHDFS.IsInputGenerateSuccess(dimension_source_site_input_dir)
+        val dimension_subject_input_dir =DataIO.getDataFrameOps.getDimensionPath(MEDUSA_DIMENSION, DimensionTypes.DIM_MEDUSA_SUBJECT)
+        val dimensionSubjectFlag = FilesInHDFS.IsInputGenerateSuccess(dimension_subject_input_dir)
+        val dimension_program_input_dir =DataIO.getDataFrameOps.getDimensionPath(MEDUSA_DIMENSION, DimensionTypes.DIM_MEDUSA_PROGRAM)
+        val dimensionProgramFlag = FilesInHDFS.IsInputGenerateSuccess(dimension_program_input_dir)
+        println(s"--------------------dimensionSourceSiteFlag is ${dimensionSourceSiteFlag}")
+        println(s"--------------------dimensionSubjectFlag is ${dimensionSubjectFlag}")
+        println(s"--------------------dimensionProgramFlag is ${dimensionProgramFlag}")
+        if (dimensionSourceSiteFlag && dimensionSubjectFlag && dimensionProgramFlag) {
+          DataIO.getDataFrameOps.getDimensionDF(sqlContext, p.paramMap, MEDUSA_DIMENSION, DimensionTypes.DIM_MEDUSA_SOURCE_SITE).registerTempTable(DimensionTypes.DIM_MEDUSA_SOURCE_SITE)
+          DataIO.getDataFrameOps.getDimensionDF(sqlContext, p.paramMap,MEDUSA_DIMENSION, DimensionTypes.DIM_MEDUSA_SUBJECT).registerTempTable(DimensionTypes.DIM_MEDUSA_SUBJECT)
+          DataIO.getDataFrameOps.getDimensionDF(sqlContext, p.paramMap,MEDUSA_DIMENSION, DimensionTypes.DIM_MEDUSA_PROGRAM).registerTempTable(DimensionTypes.DIM_MEDUSA_PROGRAM)
+        } else {
+          throw new RuntimeException(s"--------------------dimension not exist")
+        }
+        val moretv_table="moretv_table"
+        val medusa_table="medusa_table"
+
+        var sqlStr :String=""
+        val cal = Calendar.getInstance()
+        cal.setTime(DateFormatUtils.readFormat.parse(p.startDate))
+        (0 until p.numOfDays).foreach(i => {
+          val inputDate = DateFormatUtils.readFormat.format(cal.getTime)
+          val medusa_input_dir = DataIO.getDataFrameOps.getPath(MEDUSA, LogTypes.PLAY, inputDate)
+          val moretv_input_dir = DataIO.getDataFrameOps.getPath(MORETV, LogTypes.PLAYVIEW, inputDate)
+          val outputPath = DataIO.getDataFrameOps.getPath(MERGER, LogTypes.PLAY_VIEW_ETL, inputDate)
+          val medusaFlag = FilesInHDFS.IsInputGenerateSuccess(medusa_input_dir)
+          val moretvFlag = FilesInHDFS.IsInputGenerateSuccess(moretv_input_dir)
+
+          if (p.deleteOld) {
+            HdfsUtil.deleteHDFSFile(outputPath)
+          }
+          if (medusaFlag && moretvFlag) {
+            val medusaDf = DataIO.getDataFrameOps.getDF(sqlContext, p.paramMap, MEDUSA, LogTypes.PLAY, inputDate)
+            val moretvDf = DataIO.getDataFrameOps.getDF(sqlContext, p.paramMap, MORETV, LogTypes.PLAYVIEW, inputDate)
+            val medusaColumnList=medusaDf.columns.toList.filter(e => {
+              ParquetSchema.schemaArr.contains(e)
+            })
+            val medusaColNames = medusaColumnList.mkString(",")
+            val medusaColNamesWithTable = medusaColumnList.map(e=>{
+              "a."+e
+            }).mkString(",")
+            val moretvColumnList = moretvDf.columns.toList.filter(e => {
+              ParquetSchema.schemaArr.contains(e)
+            })
+            val moretvColNames = moretvColumnList.mkString(",")
+            medusaDf.registerTempTable(medusa_table)
+            moretvDf.registerTempTable(moretv_table)
+
+            //拆分出维度
+            sqlStr = s"""
+                       |select $medusaColNames,
+                       |    getEntranceType(pathMain,'medusa')    as entryType,
+                       |    getSubjectName(pathSpecial)           as subjectName,
+                       |    getSubjectCode(pathSpecial,'medusa')  as subjectCode,
+                       |    getListCategoryMedusa(pathMain,1)     as main_category,
+                       |    getListCategoryMedusa(pathMain,2)     as second_category,
+                       |    getListCategoryMedusa(pathMain,3)     as third_category
+                       |from $medusa_table
+                     """.stripMargin
+            println("--------------------" + sqlStr)
+            val medusa_table_init_table_df=sqlContext.sql(sqlStr)
+            medusa_table_init_table_df.registerTempTable("medusa_table_init_table")
+            //解析subject code维度,解析不到，使用subject维度表补全
+            sqlStr = s"""
+                        |select  $medusaColNamesWithTable,
+                        |        a.entryType,
+                        |        a.main_category,
+                        |        a.second_category,
+                        |        a.third_category,
+                        |       if((a.subjectCode is null or a.subjectCode=''),b.subject_code,a.subjectCode) as subjectCode
+                        |from medusa_table_init_table a
+                        |left join
+                        |    (select subject_name,
+                        |            first(subject_code) as subject_code
+                        |     from
+                        |     ${DimensionTypes.DIM_MEDUSA_SUBJECT}
+                        |     group by subject_name
+                        |    ) b
+                        |on trim(a.subjectName)=trim(b.subject_name)
+                     """.stripMargin
+            println("--------------------" + sqlStr)
+            sqlContext.sql(sqlStr).registerTempTable("medusa_table_init_table_2")
+
+            //过滤列表页中的脏数据[不包含'kids','sports','mv']
+            sqlStr =
+              s"""
+                 |select $medusaColNamesWithTable,
+                 |       a.entryType,
+                 |       a.main_category,
+                 |       if(b.second_category is null,'',b.second_category) as second_category,
+                 |       a.third_category,
+                 |       a.subjectCode,
+                 |       'medusa' as flag
+                 |from medusa_table_init_table_2                a
+                 |left join
+                 |    ${DimensionTypes.DIM_MEDUSA_SOURCE_SITE}  b
+                 |    on  a.main_category=b.site_content_type and a.second_category=b.second_category
+                 |    and b.main_category_code!='program_site'
+                 |where  a.main_category not in ('kids','sports','mv')
+                     """.stripMargin
+
+            //过滤列表页中的脏数据[包含'kids','sports','mv']
+            sqlStr =
+              s"""
+                 |select $medusaColNamesWithTable,
+                 |       a.entryType,
+                 |       a.main_category,
+                 |       if(b.second_category is null,'',b.second_category) as second_category,
+                 |       a.third_category,
+                 |       a.subjectCode,
+                 |       'medusa' as flag
+                 |from medusa_table_init_table_2                a
+                 |left join
+                 |    ${DimensionTypes.DIM_MEDUSA_SOURCE_SITE}  b
+                 |    on  a.main_category=b.site_content_type and a.second_category=b.second_category
+                 |    and b.main_category_code!='program_site'
+                 |where  a.main_category in ('kids','sports','mv')
+                     """.stripMargin
+
+
+
+
+
+
+
+            val medusa_rdd = sqlContext.sql(sqlStr).toJSON
+
+            val sqlSelectMoretv =s"""select $moretvColNames,
+                                     |  getEntranceType(path,'moretv')     as entryType,
+                                     |  getListCategoryMoretv(path,1)      as main_category,
+                                     |  getListCategoryMoretv(path,2)      as second_category,
+                                     |  getListCategoryMoretv(path,3)      as third_category,
+                                     |  getSubjectCode(path,'moretv')      as subjectCode,
+                                     |  'moretv'                           as flag
+                                     |from $moretv_table
+                     """.stripMargin
+            val moretv_rdd = sqlContext.sql(sqlSelectMoretv).toJSON
+            //3.x and 2.x log merge
+            val mergerRDD = medusa_rdd.union(moretv_rdd)
+            val merge_table_df = sqlContext.read.json(mergerRDD).toDF()
+            merge_table_df.cache()
+            merge_table_df.registerTempTable("merge_table")
+
+            /** 用于过滤单个用户播放单个视频量过大的情况 */
+            sqlStr =
+              s"""
+                 |select concat(userId,videoSid) as filterColumn
+                 |from merge_table
+                 |group by concat(userId,videoSid)
+                 |having count(1)>=$playNumLimit
+                     """.stripMargin
+            println("--------------------" + sqlStr)
+            sqlContext.sql(sqlStr).registerTempTable("merge_table_filter")
+
+           val mergeColNamesWithTable= merge_table_df.columns.toList.map(e=>{
+              "a."+e
+            }).mkString(",")
+            sqlStr =
+              s"""
+                 |select $mergeColNamesWithTable
+                 |from merge_table              a
+                 |     left join
+                 |     merge_table_filter       b
+                 |     on concat(a.userId,a.videoSid)=b.filterColumn
+                 |where b.filterColumn is null
+                     """.stripMargin
+            println("--------------------" + sqlStr)
+            val result_df = sqlContext.sql(sqlStr)
+            result_df.write.parquet(outputPath)
+          } else if (!medusaFlag && moretvFlag) {
+            throw new RuntimeException("medusaFlag is false")
+          } else if (medusaFlag && !moretvFlag) {
+            throw new RuntimeException("moretvFlag is false")
+          }
+          cal.add(Calendar.DAY_OF_MONTH, -1)
+        })
+      }
+      case None => {
+        throw new RuntimeException("At least needs one param: startDate!")
+      }
+    }
+  }
+}
