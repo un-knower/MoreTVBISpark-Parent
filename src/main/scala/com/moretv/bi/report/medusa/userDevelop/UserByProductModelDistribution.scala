3d61561ba221622a4849e5d9da70b62c5e27e31c
HsiaChubby <hsiachubby@gmail.com>
2017-04-17 14:37:43 +0800

    增productModel统计

---
+++ src/main/scala/com/moretv/bi/report/medusa/userDevelop/UserByProductModelDistribution.scala
@@ -0,0 +1,81 @@
+package com.moretv.bi.report.medusa.userDevelop
+
+import java.lang.{Long => JLong}
+import java.util.Calendar
+
+import cn.whaley.sdk.dataexchangeio.DataIO
+import com.moretv.bi.global.{DataBases, DimensionTypes, LogTypes}
+import com.moretv.bi.util.baseclasee.{BaseClass, ModuleClass}
+import com.moretv.bi.util.{DateFormatUtils, ParamsParseUtil}
+
+/**
+ * Created by Chubby on 2016/9/7.
+ */
+object UserByProductModelDistribution extends BaseClass {
+  private val tableName = "medusa_user_product_model_distribution"
+  private val insertSql = s"insert into ${tableName}(day,product_model,new_user_num,total_user_num) values (?,?,?,?)"
+  private val deleteSql = s"delete from ${tableName} where day = ?"
+
+  def main(args: Array[String]) {
+    ModuleClass.executor(this,args)
+  }
+
+  override def execute(args: Array[String]) {
+    ParamsParseUtil.parse(args) match {
+      case Some(p) => {
+        val util = DataIO.getMySqlOps(DataBases.MORETV_MEDUSA_MYSQL)
+        val startDate = p.startDate
+        val calendar = Calendar.getInstance()
+        calendar.setTime(DateFormatUtils.readFormat.parse(startDate))
+
+        (0 until p.numOfDays).foreach(i => {
+          val date = DateFormatUtils.readFormat.format(calendar.getTime)
+          val inputDate = DateFormatUtils.enDateAdd(date,-1)
+          val insertDate = DateFormatUtils.toDateCN(inputDate,0)
+          calendar.add(Calendar.DAY_OF_MONTH, -1)
+
+          DataIO.getDataFrameOps.getDimensionDF(sc, p.paramMap,MEDUSA_DIMENSION, DimensionTypes.DIM_MEDUSA_PRODUCT_MODEL).
+            registerTempTable(DimensionTypes.DIM_MEDUSA_PRODUCT_MODEL)
+
+          DataIO.getDataFrameOps.getDF(sc, p.paramMap, DBSNAPSHOT, LogTypes.MORETV_MTV_ACCOUNT,inputDate).
+            registerTempTable(LogTypes.MORETV_MTV_ACCOUNT)
+
+
+          /**统计新增用户的终端品牌分布*/
+
+          val newUserRdd = sqlContext.sql(
+            s"""
+              |select a.product_model,count(distinct a.mac)
+              |from ${LogTypes.MORETV_MTV_ACCOUNT} as a
+              |where a.openTime between '$insertDate 00:00:00' and '$insertDate 23:59:59'
+              |group by a.product_model
+            """.stripMargin).map(e=>(e.getString(0),e.getLong(1)))
+
+
+          /**统计累计用户的终端品牌分布*/
+          val totalUserRdd = sqlContext.sql(
+            s"""
+               |select a.product_model,count(distinct a.mac)
+               |from ${LogTypes.MORETV_MTV_ACCOUNT} as a
+               |where a.openTime <= '$insertDate 23:59:59'
+               |group by a.product_model
+            """.stripMargin).map(e=>(e.getString(0),e.getLong(1)))
+
+          if(p.deleteOld){
+            util.delete(deleteSql,insertDate)
+          }
+
+          newUserRdd.join(totalUserRdd).map(e=>(e._1,e._2._1,e._2._2)).collect().foreach(i=>{
+            util.insert(insertSql,insertDate,i._1,i._2,i._3)
+          })
+
+        })
+      }
+
+      case None => {
+        throw new RuntimeException("At least needs one param: startDate!")
+      }
+    }
+  }
+
+}
