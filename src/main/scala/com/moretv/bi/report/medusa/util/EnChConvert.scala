7ee25479a13f895702efa08482f3d930e60ec9f5
HsiaChubby <hsiachubby@gmail.com>
2017-05-24 11:29:30 +0800

    add new category

--- src/main/scala/com/moretv/bi/report/medusa/util/EnChConvert.scala
+++ src/main/scala/com/moretv/bi/report/medusa/util/EnChConvert.scala
@@ -111,7 +111,7 @@ object EnChConvert {
               case "tv_jiatinglunli" => result = "家庭伦理"
               case "tv_dushiqinggan" => result = "都市情感"
               case "tv_ztj" => result = "择天记"
-              case "tv_hls2" => result = "欢乐颂2欢乐多"
+              case "tv_hls2" => result = "欢乐颂2"
               case _ => result=info
             }
           }
