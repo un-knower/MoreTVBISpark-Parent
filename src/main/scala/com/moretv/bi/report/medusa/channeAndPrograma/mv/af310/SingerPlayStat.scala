ac9b30e569256fff3876379bf97eb47a52d5fc60
HsiaChubby <hsiachubby@gmail.com>
2017-09-18 19:26:16 +0800

    修复CMS遗留的bug

--- src/main/scala/com/moretv/bi/report/medusa/channeAndPrograma/mv/af310/SingerPlayStat.scala
+++ src/main/scala/com/moretv/bi/report/medusa/channeAndPrograma/mv/af310/SingerPlayStat.scala
@@ -14,6 +14,43 @@ import org.apache.spark.sql.functions._
   */
 object SingerPlayStat extends MvStatModel {
 
+  private val SingerBug = Map(
+    "12hjefu9stkl"->"窦唯",
+    "7ors1bjmqtij"->"杨宗纬",
+  "7ors2dik5gtu"->"谭咏麟",
+  "7ors34fhgifh"->"齐秦",
+  "7ors6k6lkmgh"->"任贤齐",
+  "7ors7ojm6kd4"->"张信哲",
+  "7orsefhje4fg"->"华晨宇",
+  "7orsij3fef2d"->"许巍",
+  "7orsjlg61bxy"->"张杰",
+  "7orsk76l5h5g"->"周华健",
+  "7orskm6l9xl7"->"张敬轩",
+  "7orsln5i9w5g"->"李克勤",
+  "7orsmo3f3fef"->"李宗盛",
+  "7orsnpiki68q"->"张震岳",
+  "7orso84gw0ef"->"张学友",
+  "7orssu3fk7qs"->"朴树",
+  "7orsuvjmruru"->"光良",
+  "7orsvxhjc3gh"->"李健",
+  "7orsw05ifhac"->"刘德华",
+  "7ovw1b6lf53f"->"迪克牛仔",
+  "ab1cbcghv0ru"->"杜德伟",
+  "c3x02def1bv0"->"周传雄",
+  "deu9efnoop8r"->"汪苏泷",
+  "o83e45g6moef"->"霍尊",
+  "o83e4ghju9ef"->"黄品源",
+  "o83e5hikijvw"->"刀郎",
+  "o83e8r5imom7"->"张智霖",
+  "o83eacfhqrbd"->"麦浚龙",
+  "o83ebd3fij7n"->"李圣杰",
+  "o83efg6lgifg"->"品冠",
+  "o83egi3ft9m7"->"曹格",
+  "o83eh6hjtvc3"->"潘玮柏",
+  "o83estg6c3uv"->"薛之谦",
+  "o83et9e5kma2"->"赵传",
+  "o83eu9ika1su"->"郑伊健")
+
   val topNum = 200
 
   tbl = "medusa_play_singer_src"
@@ -103,8 +140,9 @@ object SingerPlayStat extends MvStatModel {
 
 
     agg.collect().foreach(r => {
+      val name = if(r.get(0) == r.get(1)) singerNameRevise(r.getString(0),r.getString(1)) else r.getString(1)
       util.insert(insertSql, readDate,
-        r.get(0), r.get(1),
+        r.get(0), name,
         r.get(2), r.get(3), r.get(4),
         r.get(5), r.get(6), r.get(7), r.get(8)
       )
@@ -114,6 +152,12 @@ object SingerPlayStat extends MvStatModel {
 
   }
 
+  def singerNameRevise(sid:String, name:String):String = {
+    SingerBug.get(sid) match {
+      case Some(p) => p
+      case _ => sid
+    }
+  }
 
 }
 
