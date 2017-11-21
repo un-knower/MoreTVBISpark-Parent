3e7ea0bcfa72aa48961742e38555ef06c9effd19
HsiaChubby <hsiachubby@gmail.com>
2017-11-21 11:19:32 +0800

    添加game的专题处理数据

--- src/main/scala/com/moretv/bi/report/medusa/contentEvaluation/QuerySubjectViewInfo.scala
+++ src/main/scala/com/moretv/bi/report/medusa/contentEvaluation/QuerySubjectViewInfo.scala
@@ -20,7 +20,7 @@ import org.apache.spark.storage.StorageLevel
 object QuerySubjectViewInfo extends BaseClass {
   private val historyCollect = Array("history", "collect", "account")
   private val re = "thirdparty".r
-  private val regex="""(movie|tv|hot|kids|zongyi|comic|jilu|sports|xiqu)([0-9]+)""".r
+  private val regex="""(movie|tv|hot|kids|zongyi|comic|jilu|sports|xiqu|game)([0-9]+)""".r
   private val subjectInfoMap = CodeToNameUtils.getAllSubjectCode()
   def main(args: Array[String]) {
     ModuleClass.executor(this,args)
