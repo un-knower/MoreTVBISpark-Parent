f375fa0d204ed15ec9a4f1c654a894dc0c40f589
HsiaChubby <hsiachubby@gmail.com>
2017-12-04 14:24:58 +0800

    删除任务监控

--- src/main/scala/com/moretv/bi/util/baseclasee/ModuleClass.scala
+++ src/main/scala/com/moretv/bi/util/baseclasee/ModuleClass.scala
@@ -10,7 +10,7 @@ import com.moretv.bi.util.ParamsParseUtil
  */
 object ModuleClass {
 
-  private val emailArray = Array("xia.jun@whaley.cn")
+//  private val emailArray = Array("xia.jun@whaley.cn")
   private val timeFormat = new SimpleDateFormat("yyyy-MM-dd")
   /**
    * Function:this method is a standard module to execute task
@@ -20,37 +20,37 @@ object ModuleClass {
     //initialize global parameters
     op.init()
     //obtain task execute time
-    val executeTime = ParamsParseUtil.parse(args) match {
-      case Some(p) => p.startDate
-      case None    => getNowTime
-    }
-    val alarmFlag = ParamsParseUtil.parse(args) match {
-      case Some(p) => p.alarmFlag
-      case None => true
-    }
+//    val executeTime = ParamsParseUtil.parse(args) match {
+//      case Some(p) => p.startDate
+//      case None    => getNowTime
+//    }
+//    val alarmFlag = ParamsParseUtil.parse(args) match {
+//      case Some(p) => p.alarmFlag
+//      case None => true
+//    }
 
     //task running
-    ExceptionManage.taskRunStore(op.getClass.getName,executeTime)
+//    ExceptionManage.taskRunStore(op.getClass.getName,executeTime)
     //execute task
     try {
-      //任务start时间
-      val start_time = System.currentTimeMillis()
+//      //任务start时间
+//      val start_time = System.currentTimeMillis()
       op.execute(args)
-      //任务结束时间
-      val end_time = System.currentTimeMillis()
-      val duration = (end_time-start_time)/1000
+//      //任务结束时间
+//      val end_time = System.currentTimeMillis()
+//      val duration = (end_time-start_time)/1000
       //保存执行结果
-      ExceptionManage.taskExecuteStore(op.getClass.getName,duration,executeTime)
+//      ExceptionManage.taskExecuteStore(op.getClass.getName,duration,executeTime)
     }catch {
-      case e:Throwable =>{
+//      case e:Throwable =>{
 //        e.printStackTrace()
 //        SendMail.post(e,"[北京机房][medusa]["+op.getClass.getName+"]["+executeTime+"]任务执行失败",emailArray)
-        if(alarmFlag){
-          ExceptionManage.taskExceptionStore(op.getClass.getName,e,executeTime)
-        }
-        throw e
-      }
-        throw e
+//        if(alarmFlag){
+//          ExceptionManage.taskExceptionStore(op.getClass.getName,e,executeTime)
+//        }
+//        throw e
+//      }
+      case e:Throwable =>throw e
     }finally{
       op.destroy()
     }
